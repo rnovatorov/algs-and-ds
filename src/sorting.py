31883f42a11d9af249ee050ec37c3933470b5ab2
Suenweek <par.adise@mail.ru>
2017-12-11 00:06:14 +0300

    - Add insertion sort - Add selection sort - Add merge sort - Add bubble sort

--- src/sorting.py
+++ src/sorting.py
@@ -2,9 +2,68 @@ import random
 from heapq import heappush, heappop
 
 
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        for j in range(i):
+            if array[i] < array[j]:
+                array.insert(j, array.pop(i))
+    return array
+
+
+def selection_sort(array):
+    if len(array) <= 1:
+        return array
+
+    for i in range(len(array)):
+        m = array[i]  # Min
+        for j in range(i + 1, len(array)):
+            if array[j] < m:
+                m = array[j]
+        array[i] = m
+    return array
+
+
+def merge_sort(array):
+    # Recursion base case
+    if len(array) <= 1:
+        return array
+
+    # Divide
+    mid = int(len(array) / 2)
+    left, right = array[:mid], array[mid:]
+
+    # Conquer
+    sorted_left = merge_sort(left)
+    sorted_right = merge_sort(right)
+    return _merge(sorted_left, sorted_right)
+
+
+# TODO: Find a way to avoid list.pop(0)... deque?
+def _merge(left, right):
+    merged = []
+    while True:
+        if not left:
+            merged.extend(right)
+            return merged
+        if not right:
+            merged.extend(left)
+            return merged
+        if left[0] < right[0]:
+            merged.append(left.pop(0))
+        else:
+            merged.append(right.pop(0))
+
+
+def heap_sort(array):
+    heap = []
+    for item in array:
+        heappush(heap, item)
+    return [heappop(heap) for _ in range(len(array))]
+
+
 def quick_sort(array):
     # Recursion base case
-    if len(array) == 0 or len(array) == 1:
+    if len(array) <= 1:
         return array
 
     # Choose pivot
@@ -24,8 +83,13 @@ def quick_sort(array):
     return quick_sort(left) + center + quick_sort(right)
 
 
-def heap_sort(array):
-    heap = []
-    for item in array:
-        heappush(heap, item)
-    return [heappop(heap) for _ in range(len(array))]
+def bubble_sort(array):
+    while True:
+        swapped = False
+        for i in range(len(array) - 1):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swapped = True
+        if not swapped:
+            break
+    return array
--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,15 +1,23 @@
 from proboscis import test
 from hamcrest import assert_that, is_, equal_to
-from src.sorting import quick_sort, heap_sort
-
-
-EMPTY_ARRAY = []
-UNSORTED_ARRAY = [4, -8, 15, -16, 23, -42, -42]
-SORTED_ARRAY = [4, 8, 15, 16, 23, 42, 42]
+from src.sorting import insertion_sort, selection_sort, merge_sort, \
+                        quick_sort, heap_sort, bubble_sort
 
 
 @test(groups=["sorting"])
 class TestSorting(object):
+
+    @test
+    def insertion_sort(self):
+        self.sort_all_arrays(insertion_sort)
+
+    @test
+    def selection_sort(self):
+        self.sort_all_arrays(selection_sort)
+
+    @test
+    def merge_sort(self):
+        self.sort_all_arrays(merge_sort)
 
     @test
     def quick_sort(self):
@@ -19,28 +27,43 @@ class TestSorting(object):
     def heap_sort(self):
         self.sort_all_arrays(heap_sort)
 
+    @test
+    def bubble_sort(self):
+        self.sort_all_arrays(bubble_sort)
+
     def sort_all_arrays(self, sorting_algorithm):
         for sorting_task in [
             self.sort_empty_array,
             self.sort_unsorted_array,
-            self.sort_sorted_array
+            self.sort_sorted_array,
+            self.sort_array_of_same_elements
         ]:
             sorting_task(sorting_algorithm)
 
     def sort_empty_array(self, sorting_algorithm):
+        empty_array = []
         assert_that(
-            sorting_algorithm(EMPTY_ARRAY),
-            is_(equal_to(EMPTY_ARRAY))
+            sorting_algorithm(empty_array),
+            is_(equal_to(empty_array))
         )
 
     def sort_unsorted_array(self, sorting_algorithm):
+        unsorted_array = [4, -8, 15, -16, 23, -42, -42]
         assert_that(
-            sorting_algorithm(UNSORTED_ARRAY),
-            is_(equal_to(sorted(UNSORTED_ARRAY)))
+            sorting_algorithm(unsorted_array),
+            is_(equal_to(sorted(unsorted_array)))
         )
 
     def sort_sorted_array(self, sorting_algorithm):
+        sorted_array = [4, 8, 15, 16, 23, 42, 42]
         assert_that(
-            sorting_algorithm(SORTED_ARRAY),
-            is_(equal_to(SORTED_ARRAY))
+            sorting_algorithm(sorted_array),
+            is_(equal_to(sorted_array))
+        )
+
+    def sort_array_of_same_elements(self, sorting_algorithm):
+        array_of_same_elements = [42] * 8
+        assert_that(
+            sorting_algorithm(array_of_same_elements),
+            is_(equal_to(array_of_same_elements))
         )
