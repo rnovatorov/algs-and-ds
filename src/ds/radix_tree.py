7afa62bf1ba2d52914b58c3f2692f539bfff5c2d
Suenweek <par.adise@mail.ru>
2017-12-21 00:21:57 +0300

    - Add old RadixTree implementation and test templates for it

---
+++ src/ds/radix_tree.py
@@ -0,0 +1,49 @@
+# TODO: Add search, insert and delete
+
+
+class RadixTree(object):
+    """
+    Represents Radix Tree data structure
+    """
+    def __init__(self, word_list):
+        self.root = RadixTreeNode(word_list)
+
+    def __getitem__(self, item):
+        return self.root[item]
+
+
+class RadixTreeNode(object):
+    """
+    Represent a node in RadixTree
+    """
+    def __init__(self, word_list, depth=0):
+        self.children = {}
+        self.depth = depth
+        self._build(word_list)
+
+    def __getitem__(self, item):
+        return self.children.get(item)
+
+    def _build(self, word_list):
+        cur_fl = None  # Current first letter
+        cropped_list = []
+
+        for word in sorted(word_list):
+            # If word is not empty (last level nodes)
+            if word:
+                new_fl_needed = cur_fl != word[0]
+                if new_fl_needed:
+                    # Excluding indexing error
+                    if cropped_list:
+                        self._make_child(cur_fl, cropped_list)
+                        cropped_list = []
+                    cur_fl = word[0]
+                cropped_list.append(word[1:])
+
+        # Taking care of last letter
+        if cropped_list:
+            self._make_child(cur_fl, cropped_list)
+
+    def _make_child(self, key, word_list):
+        child = RadixTreeNode(word_list, depth=self.depth + 1)
+        self.children[key] = child
